865e6232b8b389efbc9d0533d031d493a0e6743c
Frank Lau <1254030480@qq.com>
2019-02-16 17:10:15 +0800

    更新补充区块字段,utils工具文件uintTobyte编码实现

--- block.go
+++ block.go
@@ -1,21 +1,34 @@
 package main
 
-import "crypto/sha256"
+import (
+	"crypto/sha256"
+	"time"
+)
 
 const genesisInfo = "The Times 03/Jan/2009 Chancellor on brink of second bailout for banks"
 
 type Block struct {
+	Version       uint64 //版本号
 	PrevBlockHash []byte //前区块哈希
-	Hash          []byte //当前区块哈希
+	MerKleRoot    []byte //先填写为空,后续v4版本补充
+	TimeStamp     uint64 //从1970.1.1自己秒数
+	Difficulity   uint64 //挖矿难度值,v2时使用
+	Nonce         uint64 //随机数,挖矿找的就是他
 	Data          []byte //数据，目前使用字节流，v4开始使用交易代替
+	Hash          []byte //当前区块哈希,区块中本不存在的字段,为了方便我们添加进来
 }
 
 //创建区块，对Block的每一个字段填充数据即可
 func NewBlock(data string, prevBlockHash []byte) *Block {
 	block := Block{
+		Version:       00,
 		PrevBlockHash: prevBlockHash,
-		Hash:          []byte{}, //先填充为空，后续会填充数据
+		MerKleRoot:    []byte{},
+		TimeStamp:     uint64(time.Now().Unix()),
+		Difficulity:   10, //随便写的,v2在调整
+		Nonce:         10, //同difficulity
 		Data:          []byte(data),
+		Hash:          []byte{}, //先填充为空，后续会填充数据
 	}
 	//调用setHash生成哈希值
 	block.setHash()
@@ -24,11 +37,18 @@ func NewBlock(data string, prevBlockHash []byte) *Block {
 }
 
 //实现setHash函数,我们实现一个简单的函数,来计算哈希子,没有随机数,没有难度值
-func (block *Block)setHash(){
+func (block *Block) setHash() {
 	var data []byte
-	data = append(data,block.Data...)
-	data = append(data,block.PrevBlockHash...)
+
+	//uintToByte将数字转化为[]byte{},在utils中实现
+	data = append(data, uintToByte(block.Version)...)
+	data = append(data, block.PrevBlockHash...)
+	data = append(data, block.MerKleRoot...)
+	data = append(data, uintToByte(block.TimeStamp)...)
+	data = append(data, uintToByte(block.Difficulity)...)
+	data = append(data, uintToByte(block.Nonce)...)
+	data = append(data, block.Data...)
 
 	hash /*[32]byte*/ := sha256.Sum256(data)
-	block.Hash  = hash[:]
+	block.Hash = hash[:]
 }
